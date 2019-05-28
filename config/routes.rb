4749ac3f50741fbeb38ca99322b31e4ccb913f79
James <jaimersonaraujo@gmail.com>
2019-05-28 17:22:46 -0300

    Create landing page and improve css

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'issues#index'
+  authenticated :user do
+    root 'issues#index', as: :authenticated_root
+  end
+
+  root 'public#index'
 
   resources :issues
+  resources :public, only: %i[index]
 end
